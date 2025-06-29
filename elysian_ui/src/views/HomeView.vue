e9215a6c76cd7c7941e1dec3def7487af0f32394
pancakeslmao <phucthin29@gmail.com>
2025-06-29 23:13:45 +0700

    Basic SPA setup

--- elysian_ui/src/views/HomeView.vue
+++ elysian_ui/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="home" class="">
+    <h1>Homepage</h1>
+  </div>
 </template>
